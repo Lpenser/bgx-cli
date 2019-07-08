9d567e9184514653cd7ae4c180484fe6d5a96920
fanzicai <605655>
2019-07-08 17:41:42 +0800

    config仓库地址修改

--- config/config.js
+++ config/config.js
@@ -1,6 +1,5 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.default = {
-    ARCH: 'http://192.168.1.122:3000/erp-front-project/bang-template',
-    SALE: 'http://192.168.1.122:3000/SP/AngularTemplate-Web'
+    SALE: 'http://gitlab.banggood.cn/sp/topsaletemplate-web'
 };
--- config/config.ts
+++ config/config.ts
@@ -1,5 +1,4 @@
 
 export default {
-    ARCH : 'http://192.168.1.122:3000/erp-front-project/bang-template',
-    SALE: 'http://192.168.1.122:3000/SP/AngularTemplate-Web'
+    SALE: 'http://gitlab.banggood.cn/sp/topsaletemplate-web'
 }
